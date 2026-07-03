ad8bf26f29be214049420cdd51de1f228b003aa9
plesik <pawel.lesik@opi.org.pl>
2026-07-03 14:05:03 +0200

    aa

--- app/src/test/java/com/example/licytacja/moje/BridgeBidder/LCStandard/TwoNoTrump.java
+++ app/src/test/java/com/example/licytacja/moje/BridgeBidder/LCStandard/TwoNoTrump.java
@@ -27,7 +27,7 @@ public class TwoNoTrump extends Bidder {
         return bids;
     }
 
-    private PositionCalls respond(PositionState ps) {
+    public PositionCalls respond(PositionState ps) {
         PositionCalls choices = new PositionCalls(ps);
         choices.addRules(new StaymanBidder.Stayman2NT(this)::initiateConvention);
         choices.addRules(new TransferBidder.Transfer2NT(this)::initiateConvention);
@@ -43,6 +43,9 @@ public class TwoNoTrump extends Bidder {
 
         public Iterable<CallFeature> response(PositionState ps) {
             List<CallFeature> bids = new ArrayList<>();
+            // Dodajemy instrukcję: jeśli partner zalicytuje 4NT, użyj metody openerRebidTo4NT
+            bids.add(partnerBids(Bid._4NT, this::openerRebidTo4NT));
+
             bids.add(shows(Bid._3C, ntb.respondNoGame, shape(5, 11), longestMajor(4)));
             bids.add(shows(Bid._3D, ntb.respondNoGame, shape(5, 11), longestMajor(4)));
             bids.add(shows(Bid._3H, ntb.respondNoGame, shape(5, 11)));
@@ -54,7 +57,20 @@ public class TwoNoTrump extends Bidder {
 
             bids.add(shows(Bid._4H, ntb.respondGame, shape(5, 11), betterThan(Suit.Spades)));
             bids.add(shows(Bid._4S, ntb.respondGame, shape(5, 11), betterOrEqualTo(Suit.Hearts)));
+
+            // Definicja inwitu ilościowego 4NT (np. 10-11 pkt przy otwarciu 20-21)
+            bids.add(shows(Bid._4NT, pairPoints(30, 31))); 
+            
             return bids;
+        }
+
+        private PositionCalls openerRebidTo4NT(PositionState ps) {
+            PositionCalls choices = new PositionCalls(ps);
+            // Z 21 pkt (Twoja ręka) North akceptuje inwit licytując 6NT
+            choices.addRules(shows(Bid._6NT, highCardPoints(21, 35)));
+            // Z 20 pkt North pasuje
+            choices.addRules(shows(Call.PASS, highCardPoints(20, 20)));
+            return choices;
         }
     }
 }
