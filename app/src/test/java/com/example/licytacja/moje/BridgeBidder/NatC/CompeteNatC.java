474c63fd3e1c99977f7b6555b59cd63c9e826495
plesik <pawel.lesik.85@gmail.com>
2026-07-11 18:01:00 +0200

    dom

--- app/src/test/java/com/example/licytacja/moje/BridgeBidder/NatC/CompeteNatC.java
+++ app/src/test/java/com/example/licytacja/moje/BridgeBidder/NatC/CompeteNatC.java
@@ -26,14 +26,9 @@ public class CompeteNatC extends NatC {
 
         bids.add(shows(Bid._3C, FIT_8_PLUS, pairPoints(23, 25), id("CompeteNatC.compBids _3C")));
         bids.add(shows(Bid._3D, FIT_8_PLUS, pairPoints(23, 25), id("CompeteNatC.compBids _3D")));
-        bids.add(shows(Bid._3H, FIT_8_PLUS, pairPoints(23, 25), id("CompeteNatC.compBids _3H")));
-        bids.add(shows(Bid._3S, FIT_8_PLUS, pairPoints(23, 25), id("CompeteNatC.compBids _3S")));
 
         bids.add(shows(Bid._3NT, OPPS_STOPPED, pairHighCardPoints(25, 31), id("CompeteNatC.compBids _3NT")));
         bids.add(shows(Bid._2NT, IS_OPPS_CONTRACT, OPPS_STOPPED, pairPoints(20, 24), id("CompeteNatC.compBids _2NT")));
-
-        bids.add(shows(Bid._4C, not(Gerber.APPLIES), FIT_8_PLUS, pairPoints(26, 28), id("CompeteNatC.compBids _4C")));
-        bids.add(shows(Bid._4D, FIT_8_PLUS, pairPoints(26, 28), id("CompeteNatC.compBids _4D")));
 
         bids.add(shows(Bid._5C, FIT_8_PLUS, pairPoints(29, 32), fit(Suit.Spades, false), fit(Suit.Hearts, false), id("CompeteNatC.compBids _5C")));
         bids.add(shows(Bid._5D, FIT_8_PLUS, pairPoints(29, 32), fit(Suit.Spades, false), fit(Suit.Hearts, false), id("CompeteNatC.compBids _5D")));
